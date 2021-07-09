a259f26cb393e6529e56abaa31092abac6c3adc9
Anthony W <digitalpalette@outlook.com>
2021-07-09 11:43:18 -0700

    Updated quotes
    Created the final quotes for this project.

--- js/script.js
+++ js/script.js
@@ -10,13 +10,131 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+let quotes = [
 
+  {
+    quote: 'The journey of a thousand miles begins with one step.',
+    source: 'Lao Tzu'
+    
+
+  },
+
+  {
+    quote: `Our spirit is never ending
+            I'll be the protection when you fall
+            Our spirit is never ending
+            So tell them it's us, us against the world.`,
+
+    source: 'Killswitch Engage',
+    citation: 'Us Against The World - Atonement',
+    year: '2019'
+
+},
+
+{
+  quote: `You're gonna need a bigger boat.`,
+  source: 'Matt Hooper',
+  citation:'Jaws',
+  year: '1975'
+
+},
+
+{
+  quote: 'Want to try a game of lucky hit?',
+  source: 'Ryo Hazuki',
+  citation:'Shenmue 2',
+  year: '2001'
+
+},
+
+{
+  quote: 'Rip and tear, until it is done.',
+  source: 'Seraphim',
+  citation:'DOOM',
+  year: '2016'
+
+},
+
+{
+  quote: 'Thundercats, HO!',
+  source: 'Lion-O',
+  citation:'Thundercats',
+  year: '1985'
+
+},
+
+{
+  quote: 'Great minds discuss ideas; average minds discuss things; small minds discuss people.',
+  source: 'Henry Thomas Buckle',
+  year: '1901'
+
+},
+
+{
+  quote: `We've been trying to reach you about your car's extended warranty...`,
+  source: 'Spam calls',
+  citation:'Cellphone',
+  year: '2021'
+
+},
+
+{
+  quote: 'Every man is guilty of all the good he did not do.',
+  source: 'Voltaire'
+
+},
+
+{
+  quote: 'It just works!',
+  source: 'Jensen Huang',
+  citation: 'Nvidia RTX Conference',
+  year: '2018'
+
+},
+
+{
+  quote: 'How you doin?',
+  source: 'Joey',
+  citation: 'Friends'
+
+},
+
+{
+  quote: 'Welcome to the party, pal.',
+  source: 'John McClane',
+  citation: 'Die Hard',
+  year: '1988'
+
+},
+
+{
+  quote: `It's-a-Me, Mario!`,
+  source: 'Mario',
+  citation: 'Super Mario 64',
+  year: '1996'
+
+},
+
+{
+  quote: `I'm Batman`,
+  source: 'Batman',
+  citation: 'Batman',
+  year: '1989'
+
+}
+
+
+
+];
 
 
 /***
  * `getRandomQuote` function
 ***/
+function getRandomQuote(){
+  
 
+}
 
 
 /***
